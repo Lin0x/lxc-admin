8bfd175da21f852c075419005ad69d3975fc2786
Klaus Meyer <spam@klaus-meyer.net>
2015-01-10 16:02:50 +0100

    fetch the available templates using the api

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,17 +1,6 @@
-class Template
-  include ActiveModel::Model
-
-  attr_accessor :name
-
-  DEFAULT = new(name: 'ubuntu')
-
-  PATH = '/usr/share/lxc/templates'
-
-  class << self
-    def all
-      Dir["#{PATH}/*"].collect do |file|
-        new(name: file.gsub(/#{PATH}\/lxc-/, ''))
-      end
-    end
+class Template < Resource
+  schema do
+    attribute :name , :string
+    attribute :path , :string
   end
 end
